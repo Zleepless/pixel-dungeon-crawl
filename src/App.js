69467d46e6a7e81da69b944bce22b395d9324596
Zleepless <donutsepia@gmail.com>
2020-05-09 14:31:37 +0700

    Saved

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import PixelDungeon from './PixelDungeon.js';
 
 const App = () => (
-    <div className="App">
-      <h1>Just wanna insert something</h1>
-      <PixelDungeon width={40} height={40} tilesize={16} />
-    </div>
+  <div className="App">
+    <h1>Just wanna insert something</h1>
+    <PixelDungeon width={40} height={40} tilesize={16} />
+  </div>
 );
 
 export default App;
--- src/PixelDungeon.js
+++ src/PixelDungeon.js
@@ -1,9 +1,43 @@
-import React, {useRef, useEffect} from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import InputManager from './inputManager';
+import Player from './Player';
+import World from './World';
 
 const PixelDungeon = ({ width, height, tilesize }) => {
     const canvasRef = React.useRef();
-    useEffect(()=>{
+    //const [player, setPlayer] = useState(new Player(1, 2, tilesize));
+    const [world, setWorld] = useState(new World(width, height, tilesize));
+    let inputManager = new InputManager();
+    const handleInput = (action, data) => {
+        console.log(`handle input: ${action}:${JSON.stringify(data)}`);
+        let newWorld = new World();
+        Object.assign(newWorld, world);
+        newWorld.movePlayer(data.x, data.y);
+        setWorld(newWorld);
+    };
+
+    useEffect(() => {
+        console.log('Create Map');
+        let newWorld = new World();
+        Object.assign(newWorld, world);
+        newWorld.createCellularMap();
+        setWorld(newWorld);
+    }, []);
+
+    useEffect(() => {
+        console.log('Bind Input');
+        inputManager.bindKeys();
+        inputManager.subscribe(handleInput);
+        return () => {
+            inputManager.unbindKeys();
+            inputManager.unsubscribe(handleInput);
+        }
+    })
+    useEffect(() => {
         console.log('Canvas Drawen');
+        const ctx = canvasRef.current.getContext('2d')
+        ctx.clearRect(0, 0, width * tilesize, height * tilesize);
+        world.draw(ctx);
     })
     return (
         <canvas
